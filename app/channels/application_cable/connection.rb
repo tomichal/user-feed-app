3ddfff6ad7f23fa1f836142d6f46e515ec6292ca
Michal Tomaszewski <michalt48@gmail.com>
2018-06-30 15:59:06 -0700

    id action cable connection by current user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,16 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by "current_user"
+    identified_by :current_user
 
     def connect
-      logger.add_tags 'ActionCable', "current_user"
+      self.current_user = find_verified_user
+      logger.add_tags "ActionCable", current_user.name
+    end
+
+    protected
+
+    def find_verified_user
+      User.find(cookies.signed[:user_id]) || reject_unauthorized_connection
     end
   end
 end
