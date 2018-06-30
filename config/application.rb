fd814e502b8e5d23a7a3399d175eb4fcb5a83d44
Michal Tomaszewski <michalt48@gmail.com>
2018-06-30 15:45:27 -0700

    switch to using puma in dev mode for ease of their usage

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,15 @@ module UserFeedApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    default_url_options[:host] = "user-feed-app.test"
-    config.action_cable.allowed_request_origins = ["https://#{default_url_options[:host]}"]
-    config.action_cable.url = "wss://#{default_url_options[:host]}/cable"
+    # default_url_options[:host] = "user-feed-app.test"
+    # config.action_cable.allowed_request_origins = ["https://#{default_url_options[:host]}"]
+    # config.action_cable.url = "wss://#{default_url_options[:host]}/cable"
+    config.action_cable.allowed_request_origins = ["http://localhost:3000"]
+    config.action_cable.url = "ws://localhost:3000/cable"
 
     config.autoload_paths += %W(#{config.root}/lib)
     config.time_zone = "Pacific Time (US & Canada)"
-    config.force_ssl = true
+    config.force_ssl = false
     config.ssl_options = { hsts: { subdomains: true } }
     default_url_options[:protocol] = "https://"
     config.active_support.escape_html_entities_in_json = true
