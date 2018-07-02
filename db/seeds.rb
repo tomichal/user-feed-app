73ae50a0df3c38e1686c6c8beff52205ac715160
Michal Tomaszewski <michalt48@gmail.com>
2018-07-02 07:49:12 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,31 @@
-michal = User.create(name: "Michael Thom", username: "mt", password: "test1234")
-john = User.create(name: "John Doe", username: "jd", password: "test1234")
-christina = User.create(name: "Christina Pham", username: "cp", password: "test1234", is_feed_public: true)
-bill = User.create(name: "Bill Snazz", username: "bs", password: "test1234", is_feed_public: true)
+require "forgery"
 
-john.followed_users << michal
-john.followed_users << christina
+def lorem_ipsum
+  Forgery(:lorem_ipsum).paragraphs(rand(2) + 1)
+end
 
-michal.posts.create(content: "Post 1 by Michal")
-john.posts.create(content: "Post 1 by John")
-christina.posts.create(content: "Post 1 by Christina")
-bill.posts.create(content: "Post 1 by Bill")
+[
+  { username: "mt", name: "Michael Thom", password: "pass" },
+  { username: "jd", name: "John Doe", password: "pass"  },
+  { username: "cp", name: "Christina Pham", password: "pass" , is_feed_public: true },
+  { username: "bs", name: "Bill Snazz", password: "pass" , is_feed_public: true },
+  { username: "ps", name: "Phil Smart", password: "pass" },
+  { username: "jq", name: "Jane Quick", password: "pass" },
+  { username: "af", name: "Amanda Fey", password: "pass" }
+].each do |user_attrs|
+  user = User.find_or_create_by(user_attrs.slice(:username))
+  user.attributes = user_attrs
+  user.save!
+end
 
+{ "jd" => ["mt", "cp"] }.each do |follower, follows|
+  User.find_by(username: follower).followed_users = User.where(username: follows)
+end
+
+User.all.each do |user|
+  user.posts = (rand(7) + 1).times.map do |i|
+    first_part = "Post #{i+1} by #{user.first_name} "
+    max_length = rand(Post::MAX_LENGTH - first_part.length)
+    Post.new(content: "#{first_part}#{lorem_ipsum[0..max_length]}")
+  end
+end
